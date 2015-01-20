700017b337410a638f0048f7d2545fea75c44c87
Colt <coltsteele1@gmail.com>
2015-01-20 12:36:18 -0800

    add bfs

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 var nodes = document.querySelectorAll("a")
+// var nodes = bfs();
 var i = 0;
 
 var button = document.getElementById("button");
 var orderList = document.getElementById("order");
+var menu = document.getElementById("type");
+var orderList = document.getElementById("order");
+
+
+
+function changeOrder(){
+  if(menu.value === "dfs"){
+    nodes = document.querySelectorAll("a")
+  } else {
+    nodes = bfs();
+  }
+  i = 0;
+  resetDisplay();
+}
+
+function resetDisplay() {
+  var orderList = document.getElementById("order");
+  for(var j = 0; j<nodes.length; j++){
+    nodes[j].style.backgroundColor = "white";
+    orderList.innerHTML = '';
+  }
+}
 
 
 button.onclick = next;
@@ -49,6 +72,68 @@ function next(){
   }
 }
 
+
+
+
+function bfs() {
+  var result = []
+  var queue = [document.getElementById("root")]
+  while(queue.length != 0) {
+    var current_node = queue.pop()
+    // console.log(current_node)
+
+    if(current_node.tagName == "A"){
+      result.push(current_node)
+      // console.log(result)
+    }
+
+    // console.log(current_node.children)
+
+    if(current_node.children){
+      for(var i = 0; i<current_node.children.length; i++) {
+        queue.unshift(current_node.children[i]);
+        // console.log(queue)
+      }
+    }
+  }
+  return result;
+}
+
+// def bfs(value)
+//   queue = [@root]
+//
+//   while !queue.empty?
+//     current_node = queue.pop
+//     p current_node.value
+//     return current_node if current_node.value == value
+//     queue.unshift(current_node.left) if !current_node.left.nil?
+//     queue.unshift(current_node.right) if !current_node.right.nil?
+//   end
+//   return nil
+// end
+
+function nextBFS(){
+  var orderList = document.getElementById("order");
+  var bfsOrder = bfs(orderList)
+
+  if( i > 0) {
+    nodes[i-1].style.backgroundColor = "pink";
+  }
+  nodes[i].style.backgroundColor = "red";
+  orderList.innerHTML += nodes[i].innerHTML + ", ";
+
+  if(!nodes[i+1]){
+    i = 0;
+    for(var j = 0; j<nodes.length; j++){
+      nodes[j].style.backgroundColor = "white";
+      orderList.innerHTML = '';
+    }
+  }
+  else{
+    i++;
+  }
+}
+
 function prev(){
   var orderList = document.getElementById("order");
 
@@ -69,3 +154,5 @@ function prev(){
     i--;
   }
 }
+
+bfs();
